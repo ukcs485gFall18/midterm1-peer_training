b44e58b2eb70ade48cf0ed23bddff12fc5278090
Conner Hurst <hurst3370@gmail.com>
2018-09-23 15:47:48 -0400

    Added 'Graph' button to workout page, with new view controller for graph page.

---
+++ GraphViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GraphViewController.swift
+//  Prancercise Tracker
+//
+//  Created by Hurst, Conner on 9/23/18.
+//  Copyright © 2018 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+    @IBOutlet weak var WorkoutNum: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
